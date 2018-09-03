b19b51ae3d3598fcb0e25d37630453aa5617c943
SystemError00 <Steve.Foster28@myhunter.cuny.edu>
2018-09-02 22:56:26 -0400

    Update DDRSetImage.py

--- DDRSetImage.py
+++ DDRSetImage.py
@@ -5,7 +5,7 @@ import numpy as np
 import PIL
 
 def DDRSet (First, Second, Third):
-    list_im = ['/Users/err0r/Desktop/DDRBot/images/%s.jpg' % (First), '/Users/err0r/Desktop/DDRBot/images/%s.jpg' % (Second), '/Users/err0r/Desktop/DDRBot/images/%s.jpg' % (Third)]
+    list_im = ['/__DIRECTORY__/%s.jpg' % (First), '/__DIRECTORY__/%s.jpg' % (Second), '/__DIRECTORY__/%s.jpg' % (Third)]
     imgs    = [ PIL.Image.open(i) for i in list_im ]
     # pick the image which is the smallest, and resize the others to match it (can be arbitrary image shape here)
     min_shape = sorted( [(np.sum(i.size), i.size ) for i in imgs])[0][1]
